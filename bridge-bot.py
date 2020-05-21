fe8771f2611d60a4d172f7eb38cba3cce0ff0438
Alex014 <aleksej000@gmail.com>
2020-05-21 14:41:53 +0300

    Bugs fixed, edit and delete messages

--- bridge-bot.py
+++ bridge-bot.py
@@ -14,6 +14,8 @@ class MyClient(discord.Client):
         self.__load_settings()
         self.updater = Updater(token=self.config["telegram"]["token"], use_context=True)
 
+        self.messages = {}
+
     def run(self):
         super(MyClient, self).run(self.config["discord"]["token"])
 
@@ -22,10 +24,11 @@ class MyClient(discord.Client):
             f = open('config.json', 'r')
         except IOError:
             print('No config.json file, you must create it, see readme.md !')
-        finally:
             f.close()
+        finally:
             content = f.read()
             self.config = json.loads(content)
+            f.close()
 
     @staticmethod
     def __file_type(attachment):
@@ -39,9 +42,12 @@ class MyClient(discord.Client):
             return 'o'
 
     @staticmethod
-    def __message_has_links(message):
-        res = re.search("(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", message.content)
+    def __message_has_links_txt(text):
+        res = re.search("(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?", text)
         return res is not None
+
+    def __message_has_links(self, message):
+        return self.__message_has_links_txt(message.content)
 
     @staticmethod
     def __make_message(message):
@@ -60,12 +66,14 @@ class MyClient(discord.Client):
 
         disable_web_page_preview = not self.__message_has_links(message)
 
-        if self.config["channels"][message.channel.name]:
+        if message.channel.name in self.config["channels"]:
             txt = self.__make_message(message)
             message_sent = self.updater.bot.send_message(chat_id=self.config["channels"][message.channel.name],
                                                          text=txt,
                                                          parse_mode='Markdown',
                                                          disable_web_page_preview=disable_web_page_preview)
+
+            self.messages[message.id] = message_sent.message_id
 
             for attachment in message.attachments:
                 if self.__file_type(attachment) == 'v':
@@ -79,6 +87,24 @@ class MyClient(discord.Client):
 
             return
 
+        return
+
+    async def on_message_edit(self, before, after):
+        if before.channel.name in self.config["channels"]:
+            if before.id in self.messages:
+                txt = self.__make_message(after)
+                disable_web_page_preview = not self.__message_has_links(after)
+                self.updater.bot.edit_message_text(chat_id=self.config["channels"][before.channel.name],
+                                                   message_id=self.messages[before.id],
+                                                   text=txt,
+                                                   parse_mode='Markdown',
+                                                   disable_web_page_preview=disable_web_page_preview)
+
+    async def on_message_delete(self, message):
+        if message.channel.name in self.config["channels"]:
+            if message.id in self.messages:
+                self.updater.bot.delete_message(chat_id=self.config["channels"][message.channel.name],
+                                                message_id=self.messages[message.id])
         return
 
 
